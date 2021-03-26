2f5253235269a7344571b593b16d152179413ec4
Michał Lampert <michlampert@pop-os.localdomain>
2021-03-26 04:56:59 +0100

    add small points

--- map.py
+++ map.py
@@ -10,30 +10,36 @@ class Map:
         self.walls = [(0, i-1) for i in range(w)] + [(i-1, 0) for i in range(h)] + [(h-1, i) for i in range(w)] + [(i, w-1) for i in range(h)]
         self.snake = [(random.randint(1,w-2), random.randint(1,h-2))]
         self.apple = (random.randint(1,w-2), random.randint(1,h-2))
+        self.live = 0
 
     def move(self, direction):
         """return actual poins and game_over flag"""
         next_cell = (self.snake[0][0] + direction[0], self.snake[0][1] + direction[1])
 
         if self.snake[0] in self.walls + self.snake[1:]:
-            return len(self.snake) - 1, True
+            return len(self.snake) - 1 + (100*self.live//(self.w * self.h))/100, True
+
+        if self.live == (self.w * self.h):
+            return len(self.snake) - 1 + (100*self.live//(self.w * self.h))/100, True  
 
         if self.apple in self.snake:
+            self.live = 0
             while self.apple in self.snake:
                 self.apple = (random.randint(1,self.w-2), random.randint(1,self.h-2))
         else:
             self.snake.pop()
 
         self.snake = [next_cell] + self.snake
-        return len(self.snake) - 1, False
+        self.live += 1
+        return len(self.snake) - 1 + (100*self.live//(self.w * self.h))/100, False
 
     def restart(self):
         self.snake = [(random.randint(1,self.w-2), random.randint(1,self.h-2))]
+        self.apple = (random.randint(1,self.w-2), random.randint(1,self.h-2))
+        self.live = 0
 
     def get_distances(self):
-        """
-        return list of distances to walls [^,\,<,/,v,\,>,/], snake [^,\,<,/,v,\,>,/] and apple [x,y]
-        """
+        """return list of distances to walls [^,\,<,/,v,\,>,/], snake [^,\,<,/,v,\,>,/] and apple [x,y]"""
 
         wall_distances = []
         for dir in ALL_DIRECTIONS:
@@ -56,6 +62,7 @@ class Map:
         return wall_distances + snake_distances + list(apple_distance)
 
     def calculte_expected_result(brain,w=20,h=20):
+        """return expected points result with usage of this brain"""
         result = 0
         for i in range(100):
             map = Map(w,h)
